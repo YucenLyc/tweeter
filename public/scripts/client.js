131fd51e033b77a99ded210f6c15223db389eb45
Yucen Liu <yucen.u@gmail.com>
2021-06-16 21:03:32 +0000

    Add tweetData & create functions: createTweetElement, renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ //this function takes in a tweet object and is responsible for returning a tweet <article>
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  },
+
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1623728321503
+  }
+
+]
+
+
+$(document).ready(function() {
+  console.log("Loading DOM content");
+
+  
+  renderTweets(tweetData);
+});
+
+
+function renderTweets(tweetData) {
+  const container = $('#tweet-container');
+
+  tweetData.forEach( (tweet) => {
+    const tweetElement = createTweetElement(tweet);
+    container.prepend(tweetElement);
+  })
+}
+
+function createTweetElement(tweetData) { 
+  const user = tweetData.user;
+  
+
+  const tweetElement = `
+  <article class="tweet">
+  <header>
+    <div class="userData">
+       <img class="avatar-guest" src=${user.avatars}>
+       <div class="tweeterName">${user.name}</div>
+    </div>
+     <div class="tweeterHandle">${user.handle}</div>
+  </header>
+  <p class="tweetText">${tweetData.content.text}</p>
+  <footer>
+    <div>10 days ago</div>
+    
+      <i class="fa-solid fa-heart"></i>
+      <i class="fa-solid fa-comment"></i>
+      <i class="fa-solid fa-star"></i>
+      <i class="fa-solid fa-link"></i>
+  
+  </footer>
+</article>
+`;
+
+  return tweetElement;
+}
+  
