2f250ac78560a4bc4f92df3b184b91de6c5f4fb2
Yucen Liu <yucen.u@gmail.com>
2021-06-17 03:44:32 +0000

    Add escape for dealing with potential insecure text

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- //this function takes in a tweet object and is responsible for returning a tweet <article>
+//this function takes in a tweet object and is responsible for returning a tweet <article>
 const tweetData = [
   {
     "user": {
       "name": "Newton",
       "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
   },
 
@@ -33,97 +33,101 @@ const tweetData = [
 ]
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("Loading DOM content");
 
-  
-function renderTweets(tweetData) {
-  const container = $('#tweet-container');
+  const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-  tweetData.forEach( (tweet) => {
-    const tweetElement = createTweetElement(tweet);
-    container.prepend(tweetElement);
-  })
-}
+  function renderTweets(tweetData) {
+    const container = $('#tweet-container');
 
-function createTweetElement(tweetData) { 
-  const user = tweetData.user;
-  
+    tweetData.forEach((tweet) => {
+      const tweetElement = createTweetElement(tweet);
+      container.prepend(tweetElement);
+    })
+  }
 
-  const tweetElement = `
-  <article class="tweet">
-  <header>
-    <div class="userData">
-       <img class="avatar-guest" src=${user.avatars}>
-       <div class="tweeterName">${user.name}</div>
-    </div>
-     <div class="tweeterHandle">${user.handle}</div>
-  </header>
-  <p class="tweetText">${tweetData.content.text}</p>
-  <footer>
-    <div>10 days ago</div>
+  function createTweetElement(tweetData) {
+    const user = tweetData.user;
+
+    const tweetElement = `
+    <article class="tweet">
+    <header>
+      <div class="userData">
+        <img class="avatar-guest" src=${user.avatars}>
+        <div class="tweeterName">${user.name}</div>
+      </div>
+      <div class="tweeterHandle">${user.handle}</div>
+    </header>
+    <p class="tweetText">${escape(tweetData.content.text)}</p>
+    <footer>
+      <div>10 days ago</div>
     
       <i class="fa-solid fa-heart"></i>
       <i class="fa-solid fa-comment"></i>
       <i class="fa-solid fa-star"></i>
       <i class="fa-solid fa-link"></i>
-  
-  </footer>
-</article>
-`;
+    </footer>
+  </article>
+  `;
 
-  return tweetElement;
-}
+    return tweetElement;
+  }
 
 
-//fetching tweets from the localhost page:
-function loadtweets () {
-  $.ajax({
-    type:"GET",
-    url:"/tweets",
-    dataType: "JSON"
-  }).done (data => {
-    renderTweets(data)
-  })
-  
-}
-loadtweets();
-  //renderTweets(tweetData);
+  //fetching tweets from the localhost page:
+  function loadtweets() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      dataType: "JSON"
+    }).done(data => {
+      renderTweets(data)
+    })
 
-  $('#incoming-tweet').submit(function() {
+  }
+  loadtweets();
+
+
+  $('#incoming-tweet').submit(function () {
     let $button = $("#tweet-button");
-    
+
     event.preventDefault();
     console.log("Button clicked");
     // console.log(this);
     const textValue = $("#tweet-text").val();
     console.log(textValue);
     let textlength = textValue.length;
-    if(textlength > 140) {
+    if (textlength > 140) {
       alert("Please Compose a Tweet Within 140 Characters.");
     } else if (textlength === 0) {
       alert("Writer's block? Come Back Later Please!");
     } else {
       $.ajax({
-        method:"POST",
-        url:"/tweets",
+        method: "POST",
+        url: "/tweets",
         data: $("#incoming-tweet textarea").serialize(),
       })
-      .then (function (successfulPost) {
-      return $.ajax('/tweets', {method: "GET"})
-    })
-    .then (function (allTweets) {
-      $('#incoming-tweet')[0].reset();
-      const latestTweet = [allTweets[allTweets.length - 1]];
-      renderTweets(latestTweet);
-      });
+        .then(function (successfulPost) {
+          return $.ajax('/tweets', { method: "GET" })
+        })
+        .then(function (allTweets) {
+          $('#incoming-tweet')[0].reset();
+          const latestTweet = [allTweets[allTweets.length - 1]];
+          renderTweets(latestTweet);
+          $(".counter").val(140);
+        });
     }
-    })
-    loadtweets();
-  });
+  })
 
-  
-  
+});
+
+
+
 
 
 
@@ -138,7 +142,7 @@ loadtweets();
 
 // function createTweetElement(tweetData) { 
 //   const user = tweetData.user;
-  
+
 
 //   const tweetElement = `
 //   <article class="tweet">
@@ -152,12 +156,12 @@ loadtweets();
 //   <p class="tweetText">${tweetData.content.text}</p>
 //   <footer>
 //     <div>10 days ago</div>
-    
+
 //       <i class="fa-solid fa-heart"></i>
 //       <i class="fa-solid fa-comment"></i>
 //       <i class="fa-solid fa-star"></i>
 //       <i class="fa-solid fa-link"></i>
-  
+
 //   </footer>
 // </article>
 // `;
@@ -175,6 +179,6 @@ loadtweets();
 //   }).done (data => {
 //     renderTweets(data)
 //   })
-  
+
 // }
 // loadtweets();
