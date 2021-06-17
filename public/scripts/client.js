288305017ed60d0d53aeae2e5ab06650641f00d4
Yucen Liu <yucen.u@gmail.com>
2021-06-17 01:31:21 +0000

    Refetch tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -37,35 +37,6 @@ $(document).ready(function() {
   console.log("Loading DOM content");
 
   
-  //renderTweets(tweetData);
-
-  $('#incoming-tweet').submit(function() {
-    let $button = $("#tweet-button");
-    
-    event.preventDefault();
-    console.log("Button clicked");
-    // console.log(this);
-    const textValue = $("#tweet-text").val();
-    console.log(textValue);
-    let textlength = textValue.length;
-    if(textlength > 140) {
-      alert("Please Compose a Tweet Within 140 Characters.");
-    } else if (textlength === 0) {
-      alert("Writer's block? Come Back Later Please!");
-    } else {
-      $.ajax({
-        method:"POST",
-        url:"/tweets",
-        data: $("#incoming-tweet textarea").serialize(),
-    })
-    }
-  });
-
-  loadtweets();
-  
-});
-
-
 function renderTweets(tweetData) {
   const container = $('#tweet-container');
 
@@ -117,3 +88,93 @@ function loadtweets () {
   
 }
 loadtweets();
+  //renderTweets(tweetData);
+
+  $('#incoming-tweet').submit(function() {
+    let $button = $("#tweet-button");
+    
+    event.preventDefault();
+    console.log("Button clicked");
+    // console.log(this);
+    const textValue = $("#tweet-text").val();
+    console.log(textValue);
+    let textlength = textValue.length;
+    if(textlength > 140) {
+      alert("Please Compose a Tweet Within 140 Characters.");
+    } else if (textlength === 0) {
+      alert("Writer's block? Come Back Later Please!");
+    } else {
+      $.ajax({
+        method:"POST",
+        url:"/tweets",
+        data: $("#incoming-tweet textarea").serialize(),
+      })
+      .then (function (successfulPost) {
+      return $.ajax('/tweets', {method: "GET"})
+    })
+    .then (function (allTweets) {
+      $('#incoming-tweet')[0].reset();
+      const latestTweet = [allTweets[allTweets.length - 1]];
+      renderTweets(latestTweet);
+      });
+    }
+    })
+    loadtweets();
+  });
+
+  
+  
+
+
+
+// function renderTweets(tweetData) {
+//   const container = $('#tweet-container');
+
+//   tweetData.forEach( (tweet) => {
+//     const tweetElement = createTweetElement(tweet);
+//     container.prepend(tweetElement);
+//   })
+// }
+
+// function createTweetElement(tweetData) { 
+//   const user = tweetData.user;
+  
+
+//   const tweetElement = `
+//   <article class="tweet">
+//   <header>
+//     <div class="userData">
+//        <img class="avatar-guest" src=${user.avatars}>
+//        <div class="tweeterName">${user.name}</div>
+//     </div>
+//      <div class="tweeterHandle">${user.handle}</div>
+//   </header>
+//   <p class="tweetText">${tweetData.content.text}</p>
+//   <footer>
+//     <div>10 days ago</div>
+    
+//       <i class="fa-solid fa-heart"></i>
+//       <i class="fa-solid fa-comment"></i>
+//       <i class="fa-solid fa-star"></i>
+//       <i class="fa-solid fa-link"></i>
+  
+//   </footer>
+// </article>
+// `;
+
+//   return tweetElement;
+// }
+
+
+// fetching tweets from the localhost page:
+// function loadtweets () {
+//   $.ajax({
+//     type:"GET",
+//     url:"/tweets",
+//     dataType: "JSON"
+//   }).done (data => {
+//     renderTweets(data)
+//   })
+  
+// }
+// loadtweets();
